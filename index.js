0a34457f8fe977539c904ef41a9b9fd5c3c0fc4e
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-02-14 17:47:53 +0200

    Tehtävä 3.3 puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+  
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

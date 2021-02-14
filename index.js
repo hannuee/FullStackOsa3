c5744954b131aa0e3749a80a71ad65d8ee10c346
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-02-14 19:10:02 +0200

    Tehtävä 3.6 puhelinluettelon backend step6

--- index.js
+++ index.js
@@ -42,10 +42,16 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  // Input validation:
   if (!body.name || !body.number) {
     return response.status(400).json({ 
-      error: 'Stuff missing' 
+      error: 'Name and/or number missing!' 
     })
+  }
+  if (persons.find(person => person.name === body.name)) {
+      return response.status(400).json({ 
+        error: 'name must be unique'
+      })
   }
 
   const person = {
@@ -63,4 +69,3 @@ const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
